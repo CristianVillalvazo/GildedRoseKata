0ee22b02e826152a16c82e887015c9bb348d7ef0
CristianVillalvazo <a20130503@utem.edu.mx>
2015-10-21 19:54:02 -0500

    Ingreso nuevo metodo a la clase ReduceQuality

--- GildedRoseKata/src/gildedrose/ReduceQuality.java
+++ GildedRoseKata/src/gildedrose/ReduceQuality.java
@@ -1,5 +1,28 @@
 package gildedrose;
 
 public class ReduceQuality extends QuialityPrincipal{
+	
+	 public void ReduceQualityThanNumbers(Item item) {
+	        if (item.getQuality() < 50) {
+	            item.setQuality(item.getQuality() + 1);
+	        }
+	    }
 
-}
+	    public void ReducingQuality(Item item) {
+	        if (item.getQuality() > 0) {
+	            if (item.getName() != "Sulfuras, Hand of Ragnaros") {
+	                if (item.getName() == "Conjured Mana Cake") {
+	                    item.setQuality(item.getQuality() - 2);
+	                } else {
+	                    item.setQuality(item.getQuality() - 1);
+	                }
+	            }
+	        }
+	    }
+
+	    public void Reduce(Item item, int x) {
+	        if (item.getSellIn() < x) {
+	            ReduceQualityThanNumbers(item);
+	        }
+	    }
+	}
